f76530f454374c6b95025e045f1ec4968a93dddf
WarisaraSu <68113585+WarisaraSu@users.noreply.github.com>
2020-10-14 11:59:41 +0700

    Add files via upload

---
+++ Homework9.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+using namespace std;
+void display(string name[], float price[5][3]);
+void calprice(float price[5][3]);
+int main()
+{
+	string name[5];
+	float price[5][3];
+	for (int i=0;i<5;i++)
+	{
+	cout<<"Enter Product Name : ";
+	cin>>name[i];
+	cout<<"\tPrice :";
+	cin>>price[0][i];
+	}
+	calprice(price);
+	display(name, price);
+	system("pause");
+	return 0 ;
+}
+void display(string name[], float price[5][3]) 
+{
+	cout << left << setw(15) << "No." << setw(20) << "Product" << setw(20) << "Price" << setw(20) << "Vat7%" << setw(20) << "Net" << endl; 
+	for (int i = 0; i < 5; i++) {
+	cout << left << fixed << setw(15) << i+1 << setw(20) << name[i] << setw(20)<< setprecision(2) << price[i][0] << setw(20) << setprecision(2) << price[0][i] << setw(20) << setprecision(2) << price[0][i] << endl; 
+	}
+}
+void calprice(float price[3][5])
+{
+	for (int i = 0; i < 5; i++) {
+		price[i][1] = (price[i][0] * 7/100);
+		price[i][2] = price[i][0] + price[i][1];
+	}
+}
+	
+
